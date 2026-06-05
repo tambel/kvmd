d76c1bcd0dd4ce19d32e1e186ebe7e2eaa529e50
Artur Kamalov <arttambel@gmail.com>
2026-06-05 22:38:40 +0200

    add http image download8

--- kvmd/apps/kvmd/api/redfish/msd.py
+++ kvmd/apps/kvmd/api/redfish/msd.py
@@ -145,7 +145,7 @@ class RedfishMsdApi:
             logger.info("Download image")
             await download_and_write_image(
                 image,
-                True,
+                False,
             )
             name = PurePosixPath(urlparse(image).path).name
             logger.info(f"Downloaded. Image name: {name}")
@@ -184,7 +184,7 @@ def is_http_url(s: str) -> bool:
 
 
 
-async def download_and_write_image(url: str, secure: bool, timeout: float = 60.0):
+async def download_and_write_image(url: str, insecure: bool, timeout: float = 60.0):
 
     # async def stream_write_info() -> None:
     #     assert resp is not None
